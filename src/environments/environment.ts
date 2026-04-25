518c1cd14e61b6b6d594820954dc102d48810da9
chrisiwisi <wisi.chrisi@gmail.com>
2026-04-25 19:00:54 +0200

    feat(firebase): configure environment variables for Firebase settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_PROJECT.firebaseapp.com',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_PROJECT.appspot.com',
-    messagingSenderId: 'YOUR_SENDER_ID',
-    appId: 'YOUR_APP_ID',
+    apiKey: process.env['FIREBASE_API_KEY']!,
+    authDomain: process.env['FIREBASE_AUTH_DOMAIN']!,
+    projectId: process.env['FIREBASE_PROJECT_ID']!,
+    storageBucket: process.env['FIREBASE_STORAGE_BUCKET']!,
+    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID']!,
+    appId: process.env['FIREBASE_APP_ID']!,
   },
 };
